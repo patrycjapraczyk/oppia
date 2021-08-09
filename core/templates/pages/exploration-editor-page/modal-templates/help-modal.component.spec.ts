c056f44426c07a2530777dc9c4e634af515c8f7b
patrycjapraczyk <praczyk.patrycja@gmail.com>
2021-08-09 21:23:58 +0200

    Migrated help-modal.controller to Angular

---
+++ core/templates/pages/exploration-editor-page/modal-templates/help-modal.component.spec.ts
@@ -0,0 +1,88 @@
+// Copyright 2020 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for HelpModalComponent.
+ */
+
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { NgbActiveModal, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { SiteAnalyticsService } from 'services/site-analytics.service';
+import { ContextService } from 'services/context.service';
+import { HelpModalComponent } from './help-modal.component';
+
+fdescribe('Help Modal Component', () => {
+  let fixture: ComponentFixture<HelpModalComponent>;
+  let componentInstance: HelpModalComponent;
+  let contextService: ContextService;
+  let siteAnalyticsService: SiteAnalyticsService;
+  let ngbActiveModal: NgbActiveModal;
+  let registerOpenTutorialFromHelpCenterEventSpy = null;
+  let registerVisitHelpCenterEventSpy = null;
+  let explorationId = 'exp1';
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        NgbModalModule
+      ],
+      declarations: [
+        HelpModalComponent
+      ],
+      providers: [
+        NgbActiveModal,
+        ContextService,
+        SiteAnalyticsService
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HelpModalComponent);
+    componentInstance = fixture.componentInstance;
+    contextService = TestBed.inject(ContextService);
+    siteAnalyticsService = TestBed.inject(SiteAnalyticsService);
+    ngbActiveModal = (TestBed.inject(NgbActiveModal) as unknown) as
+      jasmine.SpyObj<NgbActiveModal>;
+    spyOn(ContextService, 'getExplorationId').and.returnValue(explorationId);
+
+    registerOpenTutorialFromHelpCenterEventSpy = spyOn(
+    SiteAnalyticsService, 'registerOpenTutorialFromHelpCenterEvent');
+    registerVisitHelpCenterEventSpy = spyOn(
+      SiteAnalyticsService, 'registerVisitHelpCenterEvent');
+  });
+
+  it('should begin editor tutorial when closing the modal', () => {
+    componentInstance.beginEditorTutorial();
+
+    expect(registerOpenTutorialFromHelpCenterEventSpy)
+      .toHaveBeenCalledWith(explorationId);
+    expect(ngbActiveModal.close).toHaveBeenCalledWith('editor');
+  });
+
+  it('should begin translation tutorial when closing the modal', () => {
+    componentInstance.beginTranslationTutorial();
+
+    expect(registerOpenTutorialFromHelpCenterEventSpy)
+      .toHaveBeenCalledWith(explorationId);
+    expect(ngbActiveModal.close).toHaveBeenCalledWith('translation');
+  });
+
+  it('should dismiss modal when changing to help center', () => {
+    componentInstance.goToHelpCenter();
+
+    expect(registerVisitHelpCenterEventSpy).toHaveBeenCalledWith(explorationId);
+    expect(ngbActiveModal.dismiss).toHaveBeenCalledWith('cancel');
+  });
+});
---
+++ core/templates/pages/exploration-editor-page/modal-templates/help-modal.component.ts
@@ -0,0 +1,58 @@
+// Copyright 2020 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Component for help modal.
+ */
+
+import { Component } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ContextService } from 'services/context.service';
+import { SiteAnalyticsService } from 'services/site-analytics.service';
+
+@Component({
+  selector: 'oppia-help-modal',
+  templateUrl: './help-modal.template.html'
+})
+export class HelpModalComponent {
+  private explorationId: string;
+  private static EDITOR_TUTORIAL_MODE: string = 'editor';
+  private static TRANSLATION_TUTORIAL_MODE: string = 'translation';
+
+  constructor(
+    private ngbActiveModal: NgbActiveModal,
+    private contextService: ContextService,
+    private siteAnalytiscService: SiteAnalyticsService,
+  ) {
+    this.explorationId = this.contextService.getExplorationId();
+  }
+
+  beginEditorTutorial(): void {
+    this.siteAnalytiscService
+        .registerOpenTutorialFromHelpCenterEvent(this.explorationId);
+    this.ngbActiveModal.close(HelpModalComponent.EDITOR_TUTORIAL_MODE);
+  }
+
+  beginTranslationTutorial(): void {
+    this.siteAnalytiscService
+      .registerOpenTutorialFromHelpCenterEvent(this.explorationId);
+    this.ngbActiveModal.close(HelpModalComponent.TRANSLATION_TUTORIAL_MODE);
+  }
+
+  goToHelpCenter(): void {
+    this.siteAnalytiscService
+      .registerVisitHelpCenterEvent(this.explorationId);
+    this.ngbActiveModal.dismiss('cancel');
+  }
+}
